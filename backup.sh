df3cf3fe77b380a9b74d5f578e409f0f425b28de
retraut <retraut@gmail.com>
2017-04-18 21:43:32 +0300

    added backup.sh

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+####################################
+#
+# Backup to Ya.Disk script.
+#
+####################################
+
+
+# What to backup.
+FILES_TO_BACKUP="/var/www/$SITENAME"
+DATE=$(date +%m_%d_%y_%H_%m_%s)
+
+# Where to backup to.
+DEST_TO_STORE_BACKUP="/mnt/yandex.disk.$SITENAME/backup"
+
+# Create archive filename.
+ARCHIVE_NAME=$SITENAME-$DATE.tgz
+
+# Remove old
+# Long listing of files in $dest to check file sizes.
+echo "Remove old backups older than '$DAYS_TO_KEEP' "
+find $DEST_TO_STORE_BACKUP/site/* -type f -mmin +$DAYS_TO_KEEP |xargs rm -f
+find $DEST_TO_STORE_BACKUP/sql/*  -type f -mmin +$DAYS_TO_KEEP |xargs rm -f
+
+
+echo "Backing up $FILES_TO_BACKUP to $DEST_TO_STORE_BACKUP/site/$ARCHIVE_NAME"
+# Backup the files using tar.
+tar czf $DEST_TO_STORE_BACKUP/site/$ARCHIVE_NAME $FILES_TO_BACKUP
+
+# Dump DB via mysqldump
+mysqldump -p$MYSQL_ROOT_PASSWORD -uroot $MYSQL_DATABASE > $DEST_TO_STORE_BACKUP/sql/mysql-$ARCHIVE_NAME.sql
